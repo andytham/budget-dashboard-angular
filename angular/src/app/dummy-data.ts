2c187aa66d90a8f0b53b3467b1c8b3e08431c03c
andy <thamandyy@gmail.com>
2018-05-26 21:23:41 -0400

    dummy data

---
+++ angular/src/app/dummy-data.ts
@@ -0,0 +1,7 @@
+import { Event } from './event';
+
+export const EVENTS: Event[] = [
+	{ id: 1, name: 'starting money', net: 10000, frequency: "once" },
+	{ id: 2, name: "groceries", net: -100, frequency: "weekly" }
+
+];
---
+++ angular/src/app/event.ts
@@ -0,0 +1,6 @@
+export class Event {
+	id: number;
+	name: string;
+	net: number;
+	frequency: string;
+}
